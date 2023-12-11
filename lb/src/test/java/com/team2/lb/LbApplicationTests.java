e30f75108e4d08d80e7df8a76796d6ba4cac61fd
jinu12 <kzkju@naver.com>
2023-12-11 21:12:01 +0900

    Project Setting

---
+++ lb/src/test/java/com/team2/lb/LbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.lb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
