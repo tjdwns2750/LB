85ed1a79285c6b7347ec00a5a5a104c64b48d5d9
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2023-12-17 21:10:41 +0900

    계시판 조회를 위해 board 테이블에서 데이터 가져오기

--- lb/src/main/java/com/team2/lb/service/BoardServiceImpl.java
+++ lb/src/main/java/com/team2/lb/service/BoardServiceImpl.java
@@ -1,14 +1,23 @@
 package com.team2.lb.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.team2.lb.dao.BoardDAO;
+import com.team2.lb.vo.Board;
 
 @Service
 public class BoardServiceImpl implements BoardService{
 	
 	@Autowired
 	BoardDAO dao;
+
+	@Override
+	public ArrayList<Board> showBoardList() {
+		ArrayList<Board> boardList = dao.showBoardList();
+		return boardList;
+	}
 
 }
