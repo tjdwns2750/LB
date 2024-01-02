749b3e3fd953f02a90e5a5904f145c3bc83db16d
weekndbro <tjdwns2755@gmail.com>
2024-01-02 14:14:49 +0900

    북보드리스트 검색 및 페이징 완료

--- lb/src/main/java/com/team2/lb/service/BookBoardServiceImpl.java
+++ lb/src/main/java/com/team2/lb/service/BookBoardServiceImpl.java
@@ -1,11 +1,15 @@
 package com.team2.lb.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.team2.lb.dao.BookBoardDAO;
+import com.team2.lb.util.PageNavigator;
 import com.team2.lb.vo.BookBoard;
 
 @Service
@@ -13,7 +17,7 @@ public class BookBoardServiceImpl implements BookBoardService {
 
 	@Autowired
 	BookBoardDAO dao;
-	
+
 	@Override
 	public int registSell(BookBoard bookBoard) {
 		int reuslt = dao.registSell(bookBoard);
@@ -21,9 +25,41 @@ public class BookBoardServiceImpl implements BookBoardService {
 	}
 
 	@Override
-	public ArrayList<BookBoard> showBoardList() {
-		ArrayList<BookBoard> boardList = dao.showBoardList();
+	public PageNavigator getPageNavigator(int pagePerGroup, int countPerPage, int page, String type,
+			String searchWord) {
+		HashMap<String, String> map = getMap(type, searchWord);
+
+//		검색 키워드가 있든 없든 전체 글수를 DB로 부터 조회하기
+		int total = dao.countTotal(map);
+
+		PageNavigator navi = new PageNavigator(pagePerGroup, countPerPage, page, total);
+		return navi;
+	}
+
+	@Override
+	public ArrayList<BookBoard> showBoardList(PageNavigator navi, String type, String searchWord) {
+
+		HashMap<String, String> map = getMap(type, searchWord);
+
+//		MyBatis 에서 제공해주는 record를 관리하는 객체 RowBounds
+//		param 2개 : 1=시작레코드, 2=몇개가져올지
+		RowBounds rb = new RowBounds(navi.getStartRecord(), navi.getCountPerPage());
+
+		ArrayList<BookBoard> boardList = dao.showBoardList(map, rb);
 		return boardList;
+	}
+
+	@Override
+	public ArrayList<BookBoard> search(Map<String, String> map) {
+		ArrayList<BookBoard> boardList = dao.search(map);
+		return boardList;
+	}
+	
+	private HashMap<String, String> getMap(String type, String searchWord) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("type", type);
+		map.put("searchWord", searchWord);
+		return map;
 	}
 
 	@Override
@@ -33,19 +69,15 @@ public class BookBoardServiceImpl implements BookBoardService {
 	}
 
 	@Override
+	public int updateBoard(BookBoard bookBoard) {
+		int result = dao.updateBoard(bookBoard);
+		return result;
+	}
+
+	@Override
 	public int deleteBoard(int boardnum) {
 		int result = dao.deleteBoard(boardnum);
 		return result;
 	}
 
-	@Override
-	public int updateBoard(BookBoard bookBoard) {
-		int result = dao.updateBoard(bookBoard);
-		return result;
-	}
-	
-	
-	
-
-	
 }
