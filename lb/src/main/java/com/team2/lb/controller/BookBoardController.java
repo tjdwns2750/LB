e937670fa5eec64bc152c49bd771368fdfb32d42
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2024-01-03 18:29:19 +0900

    좋아요 기능 함

--- lb/src/main/java/com/team2/lb/controller/BookBoardController.java
+++ lb/src/main/java/com/team2/lb/controller/BookBoardController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -24,66 +26,69 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("bookBoard")
 public class BookBoardController {
-	
 
 	@Value("${user.board.page}")
 	int countPerPage;
-	
+
 	@Value("${user.board.group}")
 	int pagePerGroup;
-	
+
 	@Autowired
 	BookBoardService service;
-	
+
 	@GetMapping("pay")
 	public String pay() {
 		return "bookBoard/pay2";
 	}
-	
+
 	@GetMapping("sellBoard")
 	public String sellBoard() {
 		return "bookBoard/sellBoard";
 	}
-	
+
 	@PostMapping("registSell")
 	public String registSell(BookBoard bookBoard, @AuthenticationPrincipal UserDetails user) {
 		bookBoard.setId(user.getUsername());
 		int result = service.registSell(bookBoard);
 		return "redirect:/bookBoard/bookBoardList";
 	}
-	
+
 	@GetMapping("bookBoardList")
-	public String boardList(Model model
-								,@RequestParam(name = "page", defaultValue = "1") int page
-								, String type, String searchWord
-								) {
-		
+	public String boardList(Model model, @RequestParam(name = "page", defaultValue = "1") int page, String type,
+			String searchWord) {
+
 		PageNavigator navi = service.getPageNavigator(pagePerGroup, countPerPage, page, type, searchWord);
-		
+
 		ArrayList<BookBoard> boardList = service.showBoardList(navi, type, searchWord);
-		
+
 		model.addAttribute("navi", navi);
 		model.addAttribute("boardList", boardList);
 		model.addAttribute("type", type);
 		model.addAttribute("searchWord", searchWord);
-		
+
 		return "bookBoard/bookBoardList";
 	}
-	
+
 	@GetMapping("read")
-	public String read(@RequestParam(name = "boardnum", defaultValue = "0") int boardnum, Model model) {
+	public String read(@RequestParam(name = "boardnum", defaultValue = "0") int boardnum, Model model, @AuthenticationPrincipal UserDetails user) {
 		BookBoard bookBoard = service.readBoard(boardnum);
-		model.addAttribute("board",bookBoard);
+		model.addAttribute("board", bookBoard);
+		if(user!= null) {
+			int check = service.checkLike(boardnum, user.getUsername());
+			model.addAttribute("check" , check);
+			log.info("check {}", check);
+		}
+		
 		return "bookBoard/detailBoard";
 	}
-	
+
 	@GetMapping("update")
 	public String update(int boardnum, Model model) {
 		BookBoard bookBoard = service.readBoard(boardnum);
 		model.addAttribute("board", bookBoard);
 		return "/bookBoard/update";
 	}
-	
+
 	@PostMapping("update")
 	public String update(BookBoard bookBoard, @AuthenticationPrincipal UserDetails user) {
 		bookBoard.setId(user.getUsername());
@@ -96,18 +101,18 @@ public class BookBoardController {
 		int result = service.deleteBoard(boardnum);
 		return "redirect:/bookBoard/bookBoardList";
 	}
-	
+
 	@ResponseBody
 	@PostMapping("recommend")
 	public int recommend(int boardnum, @AuthenticationPrincipal UserDetails user) {
 		// 현재 로그인한 유저의 id를 세팅
-		String id = user.getUsername();	
-		
+		String id = user.getUsername();
+
 //		좋아요 테이블에 게시글번호, 유저아이디가 동시에 매칭되는 열이 있는지 체크 있으면 1 없음면 0
 		int check = service.checkLike(boardnum, id);
 //		좋아요 수를 담을 cnt
 		int cnt = 0;
-		
+
 		if (check == 0) {
 			log.debug("추천안했음");
 			service.addLike(boardnum, id);
@@ -121,6 +126,19 @@ public class BookBoardController {
 			cnt = service.selectCnt(boardnum);
 			return cnt;
 		}
-		
+
 	}
+
+	@ResponseBody
+	@PostMapping("likeCnt")
+	public int likeCnt(int boardnum, @AuthenticationPrincipal UserDetails user) {
+		// 현재 로그인한 유저의 id를 세팅
+		String id = user.getUsername();
+//		좋아요 테이블에 게시글번호, 유저아이디가 동시에 매칭되는 열이 있는지 체크 있으면 1 없음면 0
+		int check = service.checkLike(boardnum, id);
+
+		return check;
+
+	}
+	
 }
